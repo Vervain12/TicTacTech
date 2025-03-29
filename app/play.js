d429fac7bb5f4b71031348a119ef2da12d544b25
Confidence Uzu <113725251+uzuthedev@users.noreply.github.com>
2025-03-29 11:43:48 -0600

    Update play.js
    play page styled

--- app/play.js
+++ app/play.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function GameSelect({ navigation }) {
 
@@ -12,20 +12,28 @@ export default function GameSelect({ navigation }) {
         navigation.navigate('Game', { aiOn: false });
     }
 
-    return(
-        <View>
-            <View>
-                <View>
-                    <TouchableOpacity
-                        onPress={handleSingle}>
-                            <Text>Single Player</Text>
-                    </TouchableOpacity>
+    return (
+        <View style={styles.screen_layout}>
+            <View style={styles.screen_setup}>
+                <View style={styles.button1_box}>
+                    <View style={styles.button1_vis}>
+                        <TouchableOpacity
+                            onPress={handleSingle}>
+                            <Text style={styles.button1_txt}>
+                                Single Player
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <View>
-                    <TouchableOpacity
-                        onPress={handleTwo}>
-                            <Text>Two Player</Text>
-                    </TouchableOpacity>
+                <View style={styles.button2_box}>
+                    <View style={styles.button2_vis}>
+                        <TouchableOpacity
+                            onPress={handleTwo}>
+                            <Text style={styles.button2_txt}>
+                                Two Player
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         </View>
@@ -33,5 +41,99 @@ export default function GameSelect({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+    screen_layout: {
+        position: 'absolute',
+        flexShrink: 0,
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        rowGap: 0
+    },
 
+    screen_setup: {
+        position: 'absolute',
+        flexShrink: 0,
+        height: 932,
+        width: 430,
+        backgroundColor: "rgba(255, 255, 255, 1)",
+        display: 'flex',
+        alignItems: 'flex-start',
+        rowGap: 0
+    },
+
+    button1_box: {
+        position: 'absolute',
+        flexShrink: 0,
+        top: '21%',
+        height: 50,
+        left: '20%',
+        width: 200,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        rowGap: 0
+    },
+
+    button1_vis: {
+        position: "absolute",
+        flexShrink: 0,
+        top: 2,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor: "rgba(0, 115, 255, 1)",
+        borderRadius: 21
+    },
+
+    button1_txt: {
+        flexShrink: 0,
+        top: '7.5%',
+        right: 0,
+        bottom: 0,
+        left: 0,
+        textAlign: 'center',
+        color: 'rgba(255, 255, 255, 1)',
+        fontFamily: 'Inter',
+        fontSize: 30,
+        fontWeight: 400
+    },
+
+    button2_box: {
+        position: 'absolute',
+        flexShrink: 0,
+        top: '30%',
+        height: 50,
+        left: '20%',
+        width: 200,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        rowGap: 0
+    },
+
+    button2_vis: {
+        position: "absolute",
+        flexShrink: 0,
+        top: 2,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor: "rgba(0, 115, 255, 1)",
+        borderRadius: 21
+    },
+
+    button2_txt: {
+        flexShrink: 0,
+        top: '9%',
+        right: 0,
+        bottom: 0,
+        left: 0,
+        textAlign: 'center',
+        color: 'rgba(255, 255, 255, 1)',
+        fontFamily: 'Inter',
+        fontSize: 30,
+        fontWeight: 400
+    }
 });
